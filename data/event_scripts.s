032af19921cf9b31d697fc8ec81e8db8890f2687
gmerc <49735371+gem-merchant@users.noreply.github.com>
2021-07-18 11:38:09 -0400

    week of july 11

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1060,3 +1060,9 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/MarigoldCityDay/scripts.inc"
+
+	.include "data/maps/MarigoldCityNight/scripts.inc"
+
+	.include "data/maps/ERoute3_Day/scripts.inc"
--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -63,6 +63,9 @@
 #define MAP_UNDERWATER_ROUTE129 (54 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE105 (55 | (0 << 8))
 #define MAP_UNDERWATER_ROUTE125 (56 | (0 << 8))
+#define MAP_MARIGOLD_CITY_DAY   (57 | (0 << 8))
+#define MAP_MARIGOLD_CITY_NIGHT (58 | (0 << 8))
+#define MAP_EROUTE3_DAY         (59 | (0 << 8))
 
 // gMapGroup_IndoorLittleroot
 #define MAP_LITTLEROOT_TOWN_BRENDANS_HOUSE_1F    (0 | (1 << 8))
